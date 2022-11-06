60927b14f26afa0f891661e99e94e6fedb3c8671
Zuzanna Olszewska <zolszewska@apptension.com>
2022-11-06 21:16:57 +0100

    remove https

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'https:/next-shop-black.vercel.app',
+        hostname: 'next-shop-black.vercel.app',
         pathname: '/_next/**',
       },
       {
