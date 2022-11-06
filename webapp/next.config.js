047f2dbfcb57b2e12925e1d4cf3c2b0e7d448e13
Zuzanna Olszewska <zolszewska@apptension.com>
2022-11-06 21:25:41 +0100

    add all domains

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'next-shop-black.vercel.app',
+        hostname: '**.vercel.app',
         pathname: '/_next/**',
       },
       {
